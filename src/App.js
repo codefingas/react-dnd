458a9a1792ebba1c35b1dbe9a353a896236a399d
Ezekiel Saturday <ezekielsaturday@Doro-Macbook.local>
2020-03-10 17:43:23 +0100

    added and rendered data to manipulate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import initialData from './initial-data';
+import Column from './column';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = initialData;
+
+  render() {
+      return this.state.columnOrder.map((columnId) => {
+        const column = this.state.columns[columnId];
+        const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+        return <Column key={column.id} column={column} tasks={tasks}/>
+      })
+  }
 }
+
 
 export default App;
---
+++ src/column.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class Column extends React.Component {
+
+
+   render() {
+       
+       return this.props.column.title;
+   }
+};
+
+export default Column;
