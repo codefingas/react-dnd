85aca0665ea5d416fecbe803490982941c5bdcf1
Ezekiel Saturday <ezekielsaturday@Doro-Macbook.local>
2020-03-12 20:26:42 +0100

    can drag and drop items

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import '@atlaskit/css-reset';
-import initialData from './initial-data';
-import Column from './column';
+import React from "react";
+import "@atlaskit/css-reset";
+import { DragDropContext } from "react-beautiful-dnd";
+import initialData from "./initial-data";
+import Column from "./column";
 
 class App extends React.Component {
   state = initialData;
 
-  render() {
-      return this.state.columnOrder.map((columnId) => {
-        const column = this.state.columns[columnId];
-        const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+  onDragEnd = result => {
+    //TODO: create onDragEnd context
+  };
 
-        return <Column key={column.id} column={column} tasks={tasks}/>
-      })
+  render() {
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        {this.state.columnOrder.map(columnId => {
+          const column = this.state.columns[columnId];
+          const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+          return <Column key={column.id} column={column} tasks={tasks} />;
+        })}
+      </DragDropContext>
+    );
   }
 }
-
 
 export default App;
